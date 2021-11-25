606de61e756a17d669e7fbb1053dc0689bf06950
xiaozou <1424113865@qq.com>
2021-11-25 20:05:04 +0800

    update

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showName">点我提示学校名称</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'School',
+        data(){
+            return{
+                name:'zafu',
+                address:'hangzhou'
+            }
+        },
+        methods: {
+            showName(){
+                console.log(this.name);
+            }
+        },
+    }
+</script>
+    
+<style>
+    .demo{
+        background: orange;
+    }
+</style>
